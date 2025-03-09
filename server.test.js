d5afd6ac881d6782530c18ac64988a53538e943b
jeann <jeannguilherme30@gmail.com>
2025-03-09 20:31:29 -0300

    Adicionado Dock-compose up na pipeline

--- server.test.js
+++ server.test.js
@@ -7,6 +7,6 @@ describe("Testando o servidor", () => {
   it("Deve retornar uma mensagem na rota raiz", async () => {
     const res = await request(app).get("/");
     expect(res.status).toBe(200);
-    expect(res.text).toBe("Joga da forca está rodando!");
+    expect(res.text).toBe("Jogo da forca está rodando!"); 
   });
 });
